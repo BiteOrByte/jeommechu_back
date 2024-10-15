94404656907e4bb87f051aee545cd27b98f72f20
seyeonn <ansse17@gmail.com>
2024-10-15 10:46:14 +0900

    Spring Boot 프로젝트 초기 설정

---
+++ src/main/kotlin/com/jeommechu/JeommechuApplication.kt
@@ -0,0 +1,11 @@
+package com.jeommechu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JeommechuApplication
+
+fun main(args: Array<String>) {
+	runApplication<JeommechuApplication>(*args)
+}
---
+++ src/test/kotlin/com/jeommechu/JeommechuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jeommechu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JeommechuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
