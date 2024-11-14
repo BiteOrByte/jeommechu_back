7b429d5d558c03ee16fbeb5e03d6dbdf38429712
devyy <devyyj@gmail.com>
2024-11-14 21:34:07 +0900

    n/a - 수정 중

---
+++ src/main/kotlin/com/jeommechu/common/ApiPrefixConfig.kt
@@ -0,0 +1,15 @@
+package com.jeommechu.common
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.util.pattern.PathPatternParser
+
+@Configuration
+class ApiPrefixConfig : WebMvcConfigurer {
+
+    override fun configurePathMatch(configurer: PathMatchConfigurer) {
+        configurer.setPatternParser(PathPatternParser())
+        configurer.addPathPrefix("/api") { true }  // 모든 컨트롤러에 /api 프리픽스를 추가
+    }
+}
